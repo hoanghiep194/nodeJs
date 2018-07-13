062aac8e43f645fef14b8647de7e48a3f2d441d1
Administrator <hiepnh@runsystem.net>
2018-07-13 09:39:08 +0700

    Commit code

---
+++ NodeJs/app.js
@@ -0,0 +1,109 @@
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+server.listen(3000);
+
+var arrayUser = [];
+var arrayRoom = [];
+class User {
+    constructor(){}
+    setName(userName){
+        this.name = userName;
+        return this;
+    }
+    setAddress(address){
+        this.address = address;
+        return this;
+    }
+}
+io.on("connection", function(socket){
+    socket.on("client-send-user", function(data){
+        if(arrayUser.indexOf(data) >= 0){
+            // fails
+            socket.emit("server-send-fails");
+        } else {
+            // success
+            arrayUser.push(data);
+            console.log(arrayUser);
+            socket.UserName = data;
+            socket.emit("server-send-success", data);
+            io.sockets.emit("server-send-list-user", arrayUser);
+        }
+    });
+    socket.on("logout", function(){
+        arrayUser.splice(arrayUser.indexOf(socket.UserName), 1);
+        console.log("remove user :" + socket.UserName);
+        socket.broadcast.emit("server-send-list-user",arrayUser);
+    })
+    socket.on("user-send-data", function(data){
+        //io.sockets.emit("server-send-message", {user:socket.UserName, message:data});
+        console.log(data.msg);
+        io.sockets.in(socket.NameRoom).emit("server-send-data",
+            {
+                user: socket.UserName,
+                message: data.msg,
+                fileImage: data.file
+            }
+        );
+    });
+    socket.on("user-typing", function(){
+        console.log(socket.NameRoom);
+        io.sockets.in(socket.NameRoom).emit("server-user-typing", socket.UserName);
+    });
+    socket.on("user-stop-typing", function(){
+        io.sockets.emit("server-user-stop-typing" );
+    });
+
+    socket.on("create-room", function(data){
+
+        socket.join(data);
+        console.log(socket.adapter.rooms);
+        socket.NameRoom = data;
+        if(arrayRoom.length == 0){
+            arrayRoom.push(data);
+        } else {
+            if(arrayRoom.indexOf(data) == -1){
+                arrayRoom.push(data);
+            }
+        }
+        io.sockets.emit("server-send-room", arrayRoom);
+        socket.emit("server-send-name-room", socket.NameRoom);
+    });
+    socket.on("client-join-room", function(data){
+        console.log(socket.NameRoom);
+        socket.leave(socket.NameRoom);
+        socket.join(data);
+        console.log("------------");
+        console.log(socket.adapter.rooms);
+        socket.NameRoom = data;
+        if(arrayRoom.length == 0){
+            arrayRoom.push(data);
+        } else {
+            if(arrayRoom.indexOf(data) == -1){
+                arrayRoom.push(data);
+            }
+        }
+
+        io.sockets.emit("server-send-room", arrayRoom);
+        socket.emit("server-send-name-room", socket.NameRoom);
+    });
+});
+
+app.get("/login", function(req, res){
+    res.render("login_new");
+});
+app.post("/login", jsonParser, function(req, res){
+    console.log(req.body);
+});
---
+++ NodeJs/public/js/main_new.js
@@ -0,0 +1,98 @@
+
+(function ($) {
+    "use strict";
+
+    function toJSONString(form) {
+        var obj = {};
+        var elements = form.querySelectorAll("input, select, textarea");
+        for (var i = 0; i < elements.length; ++i) {
+            var element = elements[i];
+            var name = element.name;
+            var value = element.value;
+
+            if (name) {
+                obj[name] = value;
+            }
+        }
+
+        return JSON.stringify(obj);
+    }
+
+/*==================================================================
+[ Focus input ]*/
+$('.input100').each(function () {
+    $(this).on('blur', function () {
+        if ($(this).val().trim() != "") {
+            $(this).addClass('has-val');
+        }
+        else {
+            $(this).removeClass('has-val');
+        }
+    })
+})
+
+
+/*==================================================================
+[ Validate ]*/
+var input = $('.validate-input .input100');
+
+$('.validate-form').on('submit', function () {
+    var check = true;
+
+    for (var i = 0; i < input.length; i++) {
+        if (validate(input[i]) == false) {
+            showValidate(input[i]);
+            check = false;
+        }
+    }
+
+    return check;
+});
+
+
+$('.validate-form .input100').each(function () {
+    $(this).focus(function () {
+        hideValidate(this);
+    });
+});
+
+function validate(input) {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            return false;
+        }
+    }
+    else {
+        if ($(input).val().trim() == '') {
+            return false;
+        }
+    }
+}
+
+function showValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).addClass('alert-validate');
+}
+
+function hideValidate(input) {
+    var thisAlert = $(input).parent();
+
+    $(thisAlert).removeClass('alert-validate');
+}
+
+}) (jQuery);
+// function sendData() {
+//     var XHR = new XMLHttpRequest();
+//     var urlEncodedData = JSON.stringify($('#form').serializeObject());
+
+//     // Set up our request
+//     XHR.open('POST', 'login');
+
+//     // Add the required HTTP header for form data POST requests
+//     XHR.setRequestHeader('Content-Type', 'application/json');
+
+//     // Finally, send our data.
+//     XHR.send(urlEncodedData);
+//   }
+
