ba08510d5e0cfeee83b29134ad7f4806b5ce70de
Nguyen Hoang Hiep <hiepnh@runsystem.net>
2018-07-16 08:51:05 +0700

    commit

--- NodeJs/public/js/client-main.js
+++ NodeJs/public/js/client-main.js
@@ -1,37 +1,5 @@
 var socket = io("http://10.1.10.154:3000");
 $(document).ready(function () {
-    $('.input100').each(function () {
-        $(this).on('blur', function () {
-            if ($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })
-    });
-    /*==================================================================
-    [ Validate ]*/
-    var input = $('.validate-input .input100');
-
-    $('.validate-form').on('submit', function () {
-        var check = true;
-
-        for (var i = 0; i < input.length; i++) {
-            if (validate(input[i]) == false) {
-                showValidate(input[i]);
-                check = false;
-            }
-        }
-
-        return check;
-    });
-    $('.validate-form .input100').each(function () {
-        $(this).focus(function () {
-            hideValidate(this);
-        });
-    });
-
     // Change forms
     $(".blmd-switch-button").click(function () {
         var _this = $(this);
@@ -160,31 +128,6 @@ function fullRipper(color, time) {
         }
     }, time)
 
-}
-
-function validate(input) {
-    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-        if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            return false;
-        }
-    }
-    else {
-        if ($(input).val().trim() == '') {
-            return false;
-        }
-    }
-}
-
-function showValidate(input) {
-    var thisAlert = $(input).parent();
-
-    $(thisAlert).addClass('alert-validate');
-}
-
-function hideValidate(input) {
-    var thisAlert = $(input).parent();
-
-    $(thisAlert).removeClass('alert-validate');
 }
 
 function submitForm() {
